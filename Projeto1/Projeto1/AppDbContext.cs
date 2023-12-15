815a2413ce9c430a82fb0b3c2d1c9d93461ce15d
Rian Fonseca <128719240+RianFonseca@users.noreply.github.com>
2023-12-15 17:47:57 -0300

    Add files via upload

---
+++ Projeto1/Projeto1/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Teste
+{
+
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public int Empresa { get; internal set; }
+    }
+}
