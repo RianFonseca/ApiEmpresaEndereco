38e1f087d84f4d2919c983d7b7347a52397b290b
Rian Fonseca <128719240+RianFonseca@users.noreply.github.com>
2023-12-15 16:27:16 -0300

    Add files via upload

---
+++ Projeto1/Projeto1/Controllers/EmpresasController.cs
@@ -0,0 +1,91 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Teste
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaController : ControllerBase
+    {
+        private readonly AppDbContext _dbcontext;
+
+        public EmpresaController(AppDbContext _context)
+        {
+            _dbcontext = _context;
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarEmpresa([FromBody] Empresa context)
+        {
+            if (context == null)
+            {
+                return BadRequest("Dado Invalido");
+            }
+
+            var empresa = new Empresa(context.Id, context.NomeSocial);
+            empresa.CNPJ = context.CNPJ;
+            empresa.NomeFantasia = context.NomeFantasia;
+            _dbcontext.Empresas.Add(empresa);
+            _dbcontext.SaveChanges();
+            return CreatedAtAction(actionName: nameof(CadastrarEmpresa), new { id = empresa.Id }, empresa);
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Empresa>> ListarEmpresas()
+        {
+            var empresas = _dbcontext.Empresas.ToList();
+            return Ok(empresas);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Empresa> ObterempresaPorId(int id)
+        {
+            var empresa = _dbcontext.Empresas.Find(id);
+
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(empresa);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarEmpresa(int id, Empresa empresaAtualizada)
+        {
+            var empresa = _dbcontext.Empresas.Find(id);
+
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            empresa.Id = empresaAtualizada.Id;
+            empresa.NomeSocial = empresaAtualizada.NomeSocial;
+            empresa.NomeFantasia = empresaAtualizada.NomeFantasia;
+            empresa.CNPJ = empresaAtualizada.CNPJ;
+           
+            _dbcontext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarEmpresa(int id)
+        {
+            var empresa = _dbcontext.Empresas.Find(id);
+
+            if (empresa == null)
+            {
+                Console.WriteLine("Não encontrado!!!");
+                return NotFound();
+            }
+
+            _dbcontext.Empresas.Remove(empresa);
+            _dbcontext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ Projeto1/Projeto1/Models/Endereco.cs
@@ -0,0 +1,29 @@
+﻿namespace Teste
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public int Cep { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+        public string Cidade { get; set; }
+        public int Empresa { get; set; }
+
+        Endereco(int id, int cep, string enderecoCompleto, string bairro, int numero, string cidade, int empresa)
+        {
+            Id = id;
+            Cep = cep;
+            EnderecoCompleto = enderecoCompleto;
+            Bairro = bairro;
+            Numero = numero;
+            Cidade = cidade;
+            Empresa = empresa;
+        }
+
+        public Endereco(int empresa)
+        {
+            Empresa = empresa;
+        }
+    }
+}
