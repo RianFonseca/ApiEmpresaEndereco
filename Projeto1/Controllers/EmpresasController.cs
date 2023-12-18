c6118dc19e2066f363a5e364b1e01fa66545c2ac
Rian Fonseca <128719240+RianFonseca@users.noreply.github.com>
2023-12-18 14:57:30 -0300

    Add files via upload

---
+++ Projeto1/Controllers/EmpresasController.cs
@@ -0,0 +1,92 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Teste
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaController : ControllerBase
+    {
+        private readonly AppDbContext _dbcontext;
+
+        public EmpresaController(AppDbContext _context)
+        {
+            _dbcontext = _context;
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarEmpresa([FromBody] Empresa context)
+        {
+            if (context == null)
+            {
+                return BadRequest("Dado Invalido");
+            }
+
+            var empresa = new Empresa(context.Id, context.NomeSocial);
+            empresa.CNPJ = context.CNPJ;
+            empresa.NomeFantasia = context.NomeFantasia;
+            _dbcontext.Empresas.Add(empresa);
+            _dbcontext.SaveChanges();
+            return CreatedAtAction(actionName: nameof(CadastrarEmpresa), new { id = empresa.Id }, empresa);
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Empresa>> ListarEmpresas()
+        {
+            var empresas = _dbcontext.Empresas.ToList();
+            return Ok(empresas);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Empresa> ObterempresaPorId(int id)
+        {
+            var empresa = _dbcontext.Empresas.Find(id);
+
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(empresa);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarEmpresa(int id, Empresa empresaAtualizada)
+        {
+            var empresa = _dbcontext.Empresas.Find(id);
+
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            empresa.Id = empresaAtualizada.Id;
+            empresa.NomeSocial = empresaAtualizada.NomeSocial;
+            empresa.NomeFantasia = empresaAtualizada.NomeFantasia;
+            empresa.CNPJ = empresaAtualizada.CNPJ;
+           
+            _dbcontext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarEmpresa(int id)
+        {
+            var empresa = _dbcontext.Empresas.Find(id);
+            var endereco = _dbcontext.Enderecos.Where(element => element.Empresa == id ).First();                     
+
+            if (empresa == null)
+            {             
+                return NotFound();
+            }
+
+            _dbcontext.Empresas.Remove(empresa);
+            _dbcontext.Enderecos.Remove(endereco);
+            _dbcontext.SaveChanges();
+            return NoContent();
+        }
+    }
+
+}
---
+++ Projeto1/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Identity.Client;
+
+namespace Teste
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetRequiredService<AppDbContext>();
+                InitializeData(dbContext);
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void InitializeData(AppDbContext Dbcontext)
+        {
+            EmpresaController empresaController = new EmpresaController(Dbcontext);
+            var empresa = new Empresa(1, "Legislação Fiscal");
+            var endereco = new Endereco(empresa.Id);
+            empresa.CNPJ = 1234;
+            empresa.NomeFantasia = "Lefisc";
+            Dbcontext.Empresas.Add(empresa);
+
+            EndereçoController endereçoController = new EndereçoController(Dbcontext);
+            var endereço = new Endereco (1);
+            endereco.Id = 1;
+            endereco.Cep = 91030080;
+            endereco.EnderecoCompleto = "Av.Tapiaçu";
+            endereco.Numero = 340;
+            endereco.Bairro = "Passo d'Areia";
+            endereco.Cidade = "Porto Alegre";
+            Dbcontext.Enderecos.Add(endereco);         
+            Dbcontext.SaveChanges();
+        }  
+
+    }
+
+}
